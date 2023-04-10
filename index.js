2f6a028fe9481034cfffaf9e929d4f2ff75d75c2
alex2517360019 <64298105+alex2517360019@users.noreply.github.com>
2023-04-10 03:52:33 -0600

    1 file modified

--- index.js
+++ index.js
@@ -1,20 +1,63 @@
 // Import stylesheets
 import './style.css';
 
-import moment from 'moment';
+// Crear una función que tome dos fechas como argumentos y devuelva la cantidad de días entre esas dos fechas.
 
-const fechaInicio = moment().format('DD MM YYYY hh:mm:ss');
-const fechaFin = moment().add(61, 'days').format('DD MM YYYY hh:mm:ss');
+// import moment from 'moment';
 
-function addArrayDays(fechaInicio, fechaFin) {
-  const start = moment(fechaInicio, 'DD MM YYYY hh:mm:ss');
-  const end = moment(fechaFin, 'DD MM YYYY hh:mm:ss');
-  const days = end.diff(start, 'days');
-  const months = end.diff(start, 'months');
-  const years = end.diff(start, 'years');
-  console.log(
-    `Difference to ${days} days | months ${months} and ${years} years`
-  );
+// const fechaInicio = moment().format('DD MM YYYY hh:mm:ss');
+// const fechaFin = moment().add(61, 'days').format('DD MM YYYY hh:mm:ss');
+
+// function addArrayDays(fechaInicio, fechaFin) {
+//   const start = moment(fechaInicio, 'DD MM YYYY hh:mm:ss');
+//   const end = moment(fechaFin, 'DD MM YYYY hh:mm:ss');
+//   const days = end.diff(start, 'days');
+//   const months = end.diff(start, 'months');
+//   const years = end.diff(start, 'years');
+//   console.log(
+//     `Difference to ${days} days | months ${months} and ${years} years`
+//   );
+// }
+
+// addArrayDays(fechaInicio, fechaFin);
+
+// Crear una función que tome un array de números como argumento y devuelva un nuevo array con todos los números ordenados de menor a mayor.
+
+// const numerosDesordenados = [0,3,4,50,-99,-3,5,8,2,5,776,2,51,3,4];
+
+// function ordenar(numeros){
+//   const ordenar = numeros.sort((a, b) => a - b);
+//   console.log(ordenar);
+// }
+
+// ordenar(numerosDesordenados);
+
+// Crear una función que tome un array de strings como argumento y devuelva un nuevo array con todas las palabras que empiezan con la letra "a" sea muniscula mayuscula o con acento.
+// const regexp = /^['AaÁá']/;
+// const nombres = ['Miguel', 'ário', 'Andrea', 'Ándre', 'Alma', 'armado', 'luis'];
+
+// function startsWith(nombres) {
+//   const array = [];
+//   for (let nombre of nombres) {
+//     if (regexp.test(nombre)) {
+//       array.push(nombre);
+//     }
+//   }
+//   console.log(array);
+// }
+// startsWith(nombres);
+
+function factotial(numero) {
+  let factorial = 0;
+  if (typeof numero == 'number') {
+    for (let i = 0; i <= numero.valueOf; i++) {
+      factorial *= numero[i];
+    }
+    console.log(factorial);
+  } else {
+    throw new Error('No es un número');
+  }
+  console.log(factorial);
 }
 
-addArrayDays(fechaInicio, fechaFin);
+factotial(5);
